369f04351374d382b1154b53ca2657153e23916e
Jaroslav Žmolík <jaroslav.zmolik@outlook.com>
2023-10-17 18:18:35 +0200

    Create vacuuming state

--- CleaningRobot/Serialization/InputSerializer.cs
+++ CleaningRobot/Serialization/InputSerializer.cs
@@ -4,7 +4,7 @@ namespace CleaningRobot.Serialization;
 
 public static class InputSerializer
 {
-    public static Input Load(string input) => JsonSerializer.Deserialize<Input>(input);
+    public static Input Load(string input) => JsonSerializer.Deserialize<Input>(input) ?? throw new("JSON serialization failed");
 }
 
 public sealed record Input(string[][] map, Start start, string[] commands, int battery);
