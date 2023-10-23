543b3b8b0b5fd52e8f8b5ada2e216376ef7a1a0d
Jaroslav Žmolík <jaroslav.zmolik@outlook.com>
2023-10-23 19:52:02 +0200

    Implemented base program

--- CleaningRobot/GlobalUsings.cs
+++ CleaningRobot/GlobalUsings.cs
@@ -2,4 +2,5 @@
 
 global using System.Collections.Immutable;
 global using System.Text.Json;
+global using CleaningRobot;
 global using CleaningRobot.Model;
--- CleaningRobot/Program.cs
+++ CleaningRobot/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿if (args.Length != 2)
+{
+    Console.WriteLine("Usage: <source.json> <result.json>");
+    return;
+}
+var inputPath = args[0];
 
-Console.WriteLine("Hello, World!");
+if (!File.Exists(inputPath))
+{
+    Console.WriteLine($"Input file {inputPath} does not exist.");
+}
+
+var inputJson = File.ReadAllText(inputPath);
+var input = Serializer.Deserialize(inputJson);
+var startingState = Loader.LoadState(input);
+var finalState = Runner.Start(startingState);
+var output = Loader.SaveState(finalState);
+var outputJson = Serializer.Serialize(output);
+
+var outputPath = args[1];
+File.WriteAllText(outputPath, outputJson);
